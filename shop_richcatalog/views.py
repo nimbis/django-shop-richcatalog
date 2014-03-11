effb8b3e0c2a434fc49bfd69ad3a9f8f08ebd217
Brian Schott <brian.schott@nimbisservices.com>
2014-03-11 14:46:58 -0400

    by default, show all products on the top of the catalog

--- shop_richcatalog/views.py
+++ shop_richcatalog/views.py
@@ -1,5 +1,7 @@
 from shop.views import ShopListView, ShopDetailView
 from shop_richcatalog.models import Catalog
+from shop.models import Product
+from django.conf import settings
 
 
 class CatalogListView(ShopListView):
@@ -11,6 +13,19 @@ class CatalogListView(ShopListView):
     template_name = 'catalog/catalog_list.html'
     context_object_name = 'catalog_list'
 
+    def get_context_data(self, **kwargs):
+        '''
+        Conditionally add all products to top catalog.
+        '''
+
+        # get context data from superclass
+        ctx = super(CatalogListView, self).get_context_data(**kwargs)
+
+        if getattr(settings, 'RICHCATALOG_SHOW_ALL_PRODUCTS', True):
+            product_list = Product.objects.filter(active=True)
+            if product_list:
+                ctx.update({"product_list": product_list})
+        return ctx
 
 class CatalogDetailView(ShopDetailView):
     '''
