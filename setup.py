dff9b5106f3b0fcf148f84015762fac40d7dba60
Eric Amador <eric.amador14@gmail.com>
2015-03-04 13:34:17 -0500

    Removing pip requirement from setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-from uuid import uuid1
-
-
-# parse requirements
-reqs = parse_requirements("requirements/common.txt", session=uuid1())
 
 # setup the project
 setup(
@@ -15,8 +11,14 @@ setup(
     description="Rich catalog functionality for django shop.",
     license="BSD",
     packages=find_packages(exclude=["tests", ]),
-    install_requires=[str(x).split(' ')[0] for x in reqs],
+    install_requires=[
+        'Django',
+        'django-shop >= 0.2.0',
+        'django-cms',
+        'django-shop-richproduct',
+        'django-mptt',
+        'image',
+    ],
     zip_safe=False,
     include_package_data=True,
-    test_suite="tests.main",
 )
